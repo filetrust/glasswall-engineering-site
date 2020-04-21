ca885dda5ab7fbdc744443049cdc9992ae314c74
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-04-21 15:35:59 +0100

    Initial commit of work

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: "Glasswall Engineering",
+  tagline: "Trust every file",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Glasswall Engineering",
+      logo: {
+        alt: "Glasswall Logo",
+        src: "img/glasswall_logo_blue.png",
+      },
+      links: [
+        {
+          to: "docs/doc1",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        {
+          href: "https://medium.com/glasswall-engineering",
+          label: "Blog",
+          position: "left",
+        },
+        {
+          href: "https://github.com/filetrust",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/doc1",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/glasswall",
+            },
+          ],
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "ttps://github.com/filetrust",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/glasswallglobal",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
