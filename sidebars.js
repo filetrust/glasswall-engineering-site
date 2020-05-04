bf7284fcc7a43005866cfba852baa96f5ac8f852
Paul Gerard <paulfgerard@hotmail.com>
2020-05-04 13:55:37 +0100

    Reworking of sidebar menu

--- sidebars.js
+++ sidebars.js
@@ -4,9 +4,18 @@ module.exports = {
 		"product-descriptions/description-rebuild", "product-descriptions/description-file-type-detection"],
 	Guides:[
 	"documentation-and-guides",
-	"guides/file-rebuild-api-using-aws-sam",
-	"guides/file-analyse-using-aws-sam",
-	"guides/aws-customer-authoriser-api-authorisation-and-access"
+	{
+		"Rebuild" :[
+			"guides/file-rebuild-api-using-aws-sam",			
+		],
+		"Analysis" :[
+			"guides/file-analyse-using-aws-sam",
+			
+		],
+		"Security" :[
+			"guides/aws-customer-authoriser-api-authorisation-and-access"
+		]
+	}
 	],
     Features: ["mdx"],
   },
