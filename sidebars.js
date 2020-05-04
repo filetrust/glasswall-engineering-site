e0ab0189fff49dbd779fdc42b656de516d71bde0
Paul Gerard <paulfgerard@hotmail.com>
2020-05-04 14:03:37 +0100

    Addition of File Detection

--- sidebars.js
+++ sidebars.js
@@ -4,18 +4,20 @@ module.exports = {
 		"product-descriptions/description-rebuild", "product-descriptions/description-file-type-detection"],
 	Guides:[
 	"documentation-and-guides",
-	{
-		"Rebuild" :[
-			"guides/file-rebuild-api-using-aws-sam",			
-		],
-		"Analysis" :[
-			"guides/file-analyse-using-aws-sam",
-			
-		],
-		"Security" :[
-			"guides/aws-customer-authoriser-api-authorisation-and-access"
-		]
-	}
+		{
+			"Rebuild" :[
+				"guides/file-rebuild-api-using-aws-sam",			
+			],
+			"Analysis" :[
+				"guides/file-analyse-using-aws-sam",			
+			],
+			"File Detection" :[
+				"guides/file-type-detection-using-aws-sam",
+			],
+			"Security" :[
+				"guides/aws-customer-authoriser-api-authorisation-and-access"
+			]
+		}
 	],
     Features: ["mdx"],
   },
